c2662d0c448fc422012e71869c0e39d97ee7747f
douglas <dublandoacena@gmail.com>
2023-09-18 12:53:29 -0300

    switch case

---
+++ dart_controledefluxoSwitch.dart
@@ -0,0 +1,14 @@
+void main() {
+  var caso = 0;
+
+  switch (caso) {
+    case 1:
+      print("Caso1");
+      break;
+    case 2:
+      print("Caso 2");
+      break;
+    default:
+      print("error");
+  }
+}
